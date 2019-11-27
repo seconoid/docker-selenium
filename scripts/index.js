aec6c850422d2f63ba8ebb5920a1a6b69eebe80c
seconoid <seconsecond@gmail.com>
2019-11-27 15:18:49 +0900

    タイムゾーン変更 スクリーンショットのサンプル追加

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
+const fs = require('fs');
+const { promisify } = require('util');
 const webdriver = require("selenium-webdriver");
 const { Builder, By, until } = webdriver;
 
 const TIMEOUT_MILLISEC = 10000;
+const SCREENSHOT_PATH = 'scripts/images/'
 
 let driver;
 
-describe("Yahooポータルサイト", () => {
+describe("Google検索実行", () => {
+  // テスト実行前の処理
+  // ここではブラウザを起動している
   before(() => {
     driver = new Builder()
       .forBrowser("chrome")
@@ -13,21 +18,47 @@ describe("Yahooポータルサイト", () => {
       .build();
   });
 
+  // テスト実行後の処理
+  // ここではブラウザを終了している
   after(() => {
     return driver.quit();
   });
 
-  it("検索ボックスに入力する", async () => {
-    await driver.get("https://www.yahoo.co.jp/");
+  // テスト
+  it("検索ボックスに入力", async () => {
+    // テスト対象のページへ遷移
+    await driver.get("https://www.google.com/");
 
-    INPUT_XPATH =
-      '//*[@id="wrapper"]/header/section[1]/div/form/fieldset/span/input';
-    await driver.wait(
-      until.elementLocated(By.xpath(INPUT_XPATH)),
-      TIMEOUT_MILLISEC
-    );
+    // 検索ボックスにキーワードを入力
     await driver
-      .findElement(By.xpath(INPUT_XPATH))
-      .sendKeys("DockerでSeleniumを使う");
+      .findElement(By.name("q"))  // name属性で検索ボックス指定
+      .sendKeys("docker selenium"); // キーワードを入力
+
+    // 検索を実行
+    await driver
+      .findElement(By.name("btnK")) // name属性で検索ボタンを指定
+      .click(); // クリックを実行
+
+    // 要素の表示を待つ
+    await driver.wait(until.elementLocated(By.id('rcnt')), TIMEOUT_MILLISEC);
+
+    // スクリーンショット撮影
+    let base64 = await driver.takeScreenshot();
+    let buffer = Buffer.from(base64, 'base64');
+
+    // bufferを保存
+    let now = new Date();
+    await promisify(fs.writeFile)(SCREENSHOT_PATH + formatDate(now) + '.png', buffer);
   });
 });
+
+function formatDate(date) {
+  let yyyy = date.getFullYear();
+  let MM = ("0" + (date.getMonth() + 1)).slice(-2);
+  let dd = ("0" + date.getDate()).slice(-2);
+  let hh = ("0" + date.getHours()).slice(-2);
+  let mm = ("0" + date.getMinutes()).slice(-2);
+  let ss = ("0" + date.getSeconds()).slice(-2);
+
+  return yyyy + MM + dd + hh + mm + ss;
+}
